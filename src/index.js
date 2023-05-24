aee218b1a6cfd0e85fb89ba469eb6cd84288b9f0
Semih Gervan <semihgervan@hotmail.com>
2023-05-24 13:55:50 +0300

    S5G2 Projesi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import './less/index.less'
 
-// Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
+// // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
+// document.querySelector("h1").addEventListener("click", function (e) {
+//     alert("Bana tıkladın!")
 // });
 
 
 // Kodlar buraya gelecek!
+
+const body = document.querySelector("body");
+body.style.transition = "background 3s ease";
+const images = document.querySelectorAll("img");
+
+
+window.onscroll = function (event) {
+    const scroll = window.pageYOffset
+    if (scroll < 400) {
+        body.style.backgroundColor = "gray"
+        images.forEach(image => {
+            image.style.borderRadius = 0;
+        })
+    }
+    else if (scroll >= 400 && scroll < 600) {
+        body.style.backgroundColor = "yellow"
+        images.forEach(image => {
+            image.style.borderRadius = "100%"
+        })
+    } else {
+        body.style.backgroundColor = "#4032c98f"
+    }
+
+}
+
+document.querySelectorAll("img").forEach(resim => {
+    resim.addEventListener("mouseover", (event) => {
+        event.target.style.filter = "grayscale(100%)"
+    })
+
+    resim.addEventListener("mouseout", (event) => {
+        event.target.style.filter = "grayscale(0)"
+    })
+
+})
